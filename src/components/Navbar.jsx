1b1f75639b0cc22b2fb648bfec7d9cdfe212f916
JaimeViana <jaime.viana.jimenez@gmail.com>
2021-04-30 11:18:32 +0200

    X/arrow optimized in every screen size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar__container">
-                <a href="#" id="navbar__logo" className="hoverable">
+                <Link to={'/'} id="navbar__logo" className="hoverable">
                     ART TENDER
-                </a>
+                </Link>
                 <ul className="navbar__menu">
                     <li className="navbar__item">
                         <Link to={"/info"} className="navbar__links info hoverable">

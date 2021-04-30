05cc40ba65d9fc821166d873f613749ea019ee43
JaimeViana <jaime.viana.jimenez@gmail.com>
2021-04-30 10:13:13 +0200

    Manifest content updated (correct logo to show)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar__container">
-                <Link to={"/"} id="navbar__logo" className="hoverable">
+                <a href="#" id="navbar__logo" className="hoverable">
                     ART TENDER
-                </Link>
+                </a>
                 <ul className="navbar__menu">
                     <li className="navbar__item">
                         <Link to={"/info"} className="navbar__links info hoverable">
